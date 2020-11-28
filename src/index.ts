4f6fb832eadf62294c4e9344fe75cbdd34c3ae40
Alexandru-Paul Dumitru <alexandru.dumitru@broadcom.com>
2020-11-28 17:01:24 +0100

    split index and server and make tsc more strict

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
 import * as dotenv from 'dotenv';
-import express from 'express';
+import app from './Server';
 
 dotenv.config();
 
 if (!process.env.PORT) {
   process.exit(1);
 }
-
 const PORT = parseInt(process.env.PORT, 10);
-
-const app = express();
-
-app.get('/', (req, res) => res.send('Initial Server'));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
