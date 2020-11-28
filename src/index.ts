c9f8c52f63cd56c68d1cd254d597b62f89fba041
Alexandru-Paul Dumitru <alexandru.dumitru@broadcom.com>
2020-11-28 16:39:23 +0100

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
+import * as dotenv from 'dotenv';
 import express from 'express';
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  process.exit(1);
+}
+
+const PORT = parseInt(process.env.PORT, 10);
 
 const app = express();
 
-const PORT = 8000;
 
 app.get('/', (req, res) => res.send("Initial Server"));
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
