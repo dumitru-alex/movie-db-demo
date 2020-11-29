44f78cee7449de2a9e64207e3c5cb56a32f9914c
Alexandru-Paul Dumitru <alexandru.dumitru@broadcom.com>
2020-11-29 18:06:44 +0100

    fix body response property

--- src/routes/Comments.ts
+++ src/routes/Comments.ts
@@ -6,7 +6,7 @@ export const router = Router();
 router.get('/', async (_req, res) => {
   try {
     const comments = await CommentService.getAll();
-    res.status(200).json({ body: comments });
+    res.status(200).json({ comments: comments });
   } catch (e) {
     res.status(500).send(e.message);
   }
@@ -15,7 +15,7 @@ router.get('/', async (_req, res) => {
 router.post('/', async (req, res) => {
   try {
     const document = await CommentService.create(req.body.comment);
-    return res.status(201).json({ body: document });
+    return res.status(201).json({ comment: document });
   } catch (e) {
     return res.status(500).send(e.message);
   }
--- src/routes/Movies.ts
+++ src/routes/Movies.ts
@@ -6,7 +6,7 @@ export const router = Router();
 router.get('/', async (_req, res) => {
   try {
     const movies = await MovieService.getAll();
-    return res.status(200).json({ body: movies });
+    return res.status(200).json({ movies: movies });
   } catch (e) {
     return res.status(500).send(e.message);
   }
@@ -15,7 +15,7 @@ router.get('/', async (_req, res) => {
 router.post('/', async (req, res) => {
   try {
     const document = await MovieService.create(req.body.movie);
-    return res.status(201).json({ body: document });
+    return res.status(201).json({ movie: document });
   } catch (e) {
     return res.status(500).send(e.message);
   }
